78d1b57886acbd95c4286d14476f549e26932237
pierrick <pierrickviret@gmail.com>
2026-06-05 14:45:09 +0200

    add opt2

--- carte/op2.py
+++ carte/op2.py
@@ -1,3 +1,31 @@
+from carte.distance import distance_km
+
 
 class optimisation:
-   
+    def __init__(self, ville):
+        self.ville = ville
+
+    def optimiser_trajet(self, villes):
+        
+        villes = list(villes)
+        n = len(villes)
+        if n < 4:
+            return villes
+
+        isOptimized = True
+        while isOptimized:
+            isOptimized = False
+            
+            for i in range(1, n - 1):
+                for j in range(i + 1, n - 1):
+                    a, b = villes[i - 1], villes[i]
+                    c, d = villes[j], villes[j + 1]
+                    
+                    avant = distance_km(a, b) + distance_km(c, d)
+                    apres = distance_km(a, c) + distance_km(b, d)
+                    if apres < avant:
+                        villes[i:j + 1] = list(reversed(villes[i:j + 1]))
+                        isOptimized = True
+                        break
+
+        return villes
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from carte import add_markers, create_map, draw_route
 from carte.distance import distance_km
+from carte.op2 import optimisation
 from carte.ville import Ville
 from data import POINTS
 
@@ -11,11 +12,21 @@ add_markers(carte, POINTS)
 ville = Ville(POINTS)
 points = ville.trajet_voisins(depart="Mairie")
 
-for i in range(len(points) - 1):
-    draw_route(carte, [points[i], points[i+1]])
-
 for i in range(len(points)):
     print(points[i][2])
+
+print("\nOptimisation :")
+
+
+pointsOptimised = optimisation(ville).optimiser_trajet(points)
+
+for i in range(len(pointsOptimised)):
+    print(pointsOptimised[i][2])
+
+for i in range(len(pointsOptimised) - 1):
+    draw_route(carte, [pointsOptimised[i], pointsOptimised[i+1]])
+
+
 
 # Exporte une carte HTML interactive
 carte.save("carte.html")
